0d14b3baf9afffd69f8a9b84e4c2603261ce661c
Antopasquettin90 <119460334+Antopasquettin90@users.noreply.github.com>
2023-05-31 20:44:50 -0300

    app solucionado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Cards from './components/Cards/Cards';
-import ItemList from './components/Items/ItemList';
-import ProductDetails from './components/Products/ProductDetails';
-import ItemDetailsProduct from './components/Products/ItemDetailsProduct';
+import Carrito from './components/Cart/Carrito';
+import CartProvider from './components/Cart/CartContext';
 import Footer from './components/Footer/Footer';
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/Inicio" element={<Cards />} />
-        <Route path="/" element={<Cards />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/product/:id/details" element={<ItemDetailsProduct />} />
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/Inicio" element={<Cards />} />
+          <Route path="/category/:categoryId" element={<Cards />} />
+          <Route path="/carrito" element={<Carrito />} />
         </Routes>
-      <Footer/>
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
